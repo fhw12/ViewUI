f4c40fcd5ddf3f23948e95a281cd5fe9d54bc904
fhw12 <nikita.pymitron@gmail.com>
2022-11-25 10:00:49 +0700

    circle - bc_hover

--- UI/circle.lua
+++ UI/circle.lua
@@ -8,7 +8,10 @@ return function(var, lib, t, i)
 	if t[i].padding then r = r - t[i].padding end
 
 	if math.pow(math.abs(var.x+var.width/2 - lib.events.mx), 2) + math.pow(math.abs(var.y+var.height/2 - lib.events.my), 2) <= r * r then
-		love.graphics.setColor(1,1,1,0.1)
+		if t[i].bc_hover then
+			love.graphics.setColor(t[i].bc_hover)
+		end
+
 		if t[i].click and lib.events.click then
 			t[i].click(t[i])
 		end
--- examples/calc.lua
+++ examples/calc.lua
@@ -59,25 +59,25 @@ app.view = {
 		{'row', size=0.6, bc={0.16,0.21,0.22}, content={
 			{'row', size=0.2, bc={0,0,0,0}, content={
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='AC', content={
-					{'circle', bc={0.43, 0.5, 0.49}, padding=3, content={
+					{'circle', bc={0.43, 0.5, 0.49}, bc_hover={1,1,1,0.2}, padding=3, content={
 						{'text', fg={0.98,0.99,1}, text="AC"}
 					}}
 				}},
 
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='<', content={
-					{'circle', bc={0.43, 0.5, 0.49}, padding=3, content={
+					{'circle', bc={0.43, 0.5, 0.49}, bc_hover={1,1,1,0.2}, padding=3, content={
 						{'text', fg={0.98,0.99,1}, text="<"}
 					}}
 				}},
 
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='%', content={
-					{'circle', bc={1,1,1}, padding=3, content={
+					{'circle', bc={1,1,1}, padding=3, bc_hover={1,1,1,0.2}, content={
 						{'text', fg={0,0.6,0.53}, text="%"}
 					}}
 				}},
 
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='/', content={
-					{'circle', bc={1,1,1}, padding=3, content={
+					{'circle', bc={1,1,1}, padding=3, bc_hover={1,1,1,0.2}, content={
 						{'text', fg={0,0.6,0.53}, text="/"}
 					}}
 				}},
@@ -85,25 +85,25 @@ app.view = {
 
 			{'row', size=0.2, bc={0,0,0,0}, content={
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='7', content={
-					{'circle', bc={0,0,0,0}, padding=3, content={
+					{'circle', bc={0,0,0,0}, padding=3, bc_hover={1,1,1,0.2}, content={
 						{'text', fg={1,1,1}, text="7"}
 					}}
 				}},
 
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='8', content={
-					{'circle', bc={0,0,0,0}, padding=3, content={
+					{'circle', bc={0,0,0,0}, padding=3, bc_hover={1,1,1,0.2}, content={
 						{'text', fg={1,1,1}, text="8"}
 					}}
 				}},
 
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='9', content={
-					{'circle', bc={0,0,0,0}, padding=3, content={
+					{'circle', bc={0,0,0,0}, padding=3, bc_hover={1,1,1,0.2}, content={
 						{'text', fg={1,1,1}, text="9"}
 					}}
 				}},
 
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='*', content={
-					{'circle', bc={1,1,1}, padding=3, content={
+					{'circle', bc={1,1,1}, padding=3, bc_hover={1,1,1,0.2}, content={
 						{'text', fg={0,0.6,0.53}, text="X"}
 					}}
 				}},
@@ -111,25 +111,25 @@ app.view = {
 
 			{'row', size=0.2, bc={0,0,0,0}, content={
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='4', padding=3, content={
-					{'circle', bc={0,0,0,0}, content={
+					{'circle', bc={0,0,0,0}, bc_hover={1,1,1,0.2}, content={
 						{'text', fg={1,1,1}, text="4"}
 					}}
 				}},
 
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='5', content={
-					{'circle', bc={0,0,0,0}, padding=3, content={
+					{'circle', bc={0,0,0,0}, bc_hover={1,1,1,0.2}, padding=3, content={
 						{'text', fg={1,1,1}, text="5"}
 					}}
 				}},
 
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='6', content={
-					{'circle', bc={0,0,0,0}, padding=3, content={
+					{'circle', bc={0,0,0,0}, bc_hover={1,1,1,0.2}, padding=3, content={
 						{'text', fg={1,1,1}, text="6"}
 					}}
 				}},
 
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='-', content={
-					{'circle', bc={1,1,1}, padding=3, content={
+					{'circle', bc={1,1,1}, bc_hover={1,1,1,0.2}, padding=3, content={
 						{'text', fg={0,0.6,0.53}, text="-"}
 					}}
 				}},
@@ -137,25 +137,25 @@ app.view = {
 
 			{'row', size=0.2, bc={0,0,0,0}, content={
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='1', content={
-					{'circle', bc={0,0,0,0}, padding=3, content={
+					{'circle', bc={0,0,0,0}, bc_hover={1,1,1,0.2}, padding=3, content={
 						{'text', fg={1,1,1}, text="1"}
 					}}
 				}},
 
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='2', content={
-					{'circle', bc={0,0,0,0}, padding=3, content={
+					{'circle', bc={0,0,0,0}, bc_hover={1,1,1,0.2}, padding=3, content={
 						{'text', fg={1,1,1}, text="2"}
 					}}
 				}},
 
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='3', content={
-					{'circle', bc={0,0,0,0}, padding=3, content={
+					{'circle', bc={0,0,0,0}, bc_hover={1,1,1,0.2}, padding=3, content={
 						{'text', fg={1,1,1}, text="3"}
 					}}
 				}},
 
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='+', content={
-					{'circle', bc={1,1,1}, padding=3, content={
+					{'circle', bc={1,1,1}, bc_hover={1,1,1,0.2}, padding=3, content={
 						{'text', fg={0,0.6,0.53}, text="+"}
 					}}
 				}},
@@ -163,25 +163,25 @@ app.view = {
 
 			{'row', size=0.2, bc={0,0,0,0}, content={
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='+-', content={
-					{'circle', bc={0,0,0,0}, padding=3, content={
+					{'circle', bc={0,0,0,0}, bc_hover={1,1,1,0.2}, padding=3, content={
 						{'text', fg={1,1,1}, text="+ / -"}
 					}}
 				}},
 
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='0', content={
-					{'circle', bc={0,0,0,0}, padding=3, content={
+					{'circle', bc={0,0,0,0}, bc_hover={1,1,1,0.2}, padding=3, content={
 						{'text', fg={1,1,1}, text="0"}
 					}}
 				}},
 
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='.', content={
-					{'circle', bc={0,0,0,0}, padding=3, content={
+					{'circle', bc={0,0,0,0}, bc_hover={1,1,1,0.2}, padding=3, content={
 						{'text', fg={1,1,1}, text="."}
 					}}
 				}},
 
 				{'column', size=1/4, bc={0,0,0,0}, click=app.button_click, text='=', content={
-					{'circle', bc={1,1,1}, padding=3, content={
+					{'circle', bc={1,1,1}, bc_hover={1,1,1,0.2}, padding=3, content={
 						{'text', fg={0,0.6,0.53}, text="="}
 					}}
 				}},
